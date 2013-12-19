55eda0305bc43097c3d1de3882cdc04dc394b3bc
Douglas Campos <qmx@qmx.me>
2013-12-19 01:24:21 -0200

    checkpoint

---
+++ Gruntfile.js
@@ -0,0 +1,38 @@
+'use strict';
+
+module.exports = function (grunt) {
+
+    require('load-grunt-tasks')(grunt);
+    require('time-grunt')(grunt);
+
+    grunt.initConfig({
+        
+        call4paperz: {
+            app: 'app',
+            dist: 'dist'
+        },
+
+        // Empties folders to start fresh
+        clean: {
+            dist: {
+                files: [{
+                    dot: true,
+                    src: [
+                        '.tmp',
+                        '<%= call4paperz.dist %>/*',
+                        '!<%= call4paperz.dist %>/.git*'
+                    ]
+                }]
+            }
+        }
+    });
+
+
+    grunt.registerTask('build', [
+        'clean:dist'
+    ]);
+
+    grunt.registerTask('default', [
+        'build'
+    ]);
+};
